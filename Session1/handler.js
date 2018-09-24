50bec7d6764940c2458593dd05e4eacd371e0fc6
Rhiannon Fletcher <rfletcher@deloitte.co.nz>
2018-09-25 11:53:53 +1200

    Removing boilerplate unnecessary code

--- Session1/handler.js
+++ Session1/handler.js
@@ -1,86 +1,6 @@
 'use strict';
 
- /**
-  * This sample demonstrates an implementation of the Lex Code Hook Interface
-  * in order to serve a sample bot which manages orders for flowers.
-  * Bot, Intent, and Slot models which are compatible with this sample can be found in the Lex Console
-  * as part of the 'OrderFlowers' template.
-  *
-  * For instructions on how to set up and test this bot, as well as additional samples,
-  *  visit the Lex Getting Started documentation.
-  */
-
-
- // --------------- Helpers to build responses which match the structure of the necessary dialog actions -----------------------
-
-function elicitSlot(sessionAttributes, intentName, slots, slotToElicit, message) {
-    return {
-        sessionAttributes,
-        dialogAction: {
-            type: 'ElicitSlot',
-            intentName,
-            slots,
-            slotToElicit,
-            message,
-        },
-    };
-}
-
-function close(sessionAttributes, fulfillmentState, message) {
-    return {
-        sessionAttributes,
-        dialogAction: {
-            type: 'Close',
-            fulfillmentState,
-            message,
-        },
-    };
-}
-
-function delegate(sessionAttributes, slots) {
-    return {
-        sessionAttributes,
-        dialogAction: {
-            type: 'Delegate',
-            slots,
-        },
-    };
-}
-
-// ---------------- Helper Functions --------------------------------------------------
-
-function parseLocalDate(date) {
-    /**
-     * Construct a date object in the local timezone by parsing the input date string, assuming a YYYY-MM-DD format.
-     * Note that the Date(dateString) constructor is explicitly avoided as it may implicitly assume a UTC timezone.
-     */
-    const dateComponents = date.split(/\-/);
-    return new Date(dateComponents[0], dateComponents[1] - 1, dateComponents[2]);
-}
-
-function isValidDate(date) {
-    try {
-        return !(isNaN(parseLocalDate(date).getTime()));
-    } catch (err) {
-        return false;
-    }
-}
-
-function buildValidationResult(isValid, violatedSlot, messageContent) {
-    if (messageContent == null) {
-        return {
-            isValid,
-            violatedSlot,
-        };
-    }
-    return {
-        isValid,
-        violatedSlot,
-        message: { contentType: 'PlainText', content: messageContent },
-    };
-}
  // --------------- Functions that control the bot's behavior -----------------------
-
 /**
  * Performs dialog management and fulfillment for greeting user.
  *
@@ -90,18 +10,20 @@ function greetUser(intent_request, callback){
     var output_session_attributes = {
         "Name" : user_name
     }
-    
-    callback(close(output_session_attributes, 'Fulfilled', 
+
+    callback(close(output_session_attributes, 'Fulfilled',
     { contentType: 'PlainText', content: selectGreeting(user_name)}));
 }
-    
-    
+
+
 function selectGreeting(user_name){
-    var options = [`Hi there ${user_name}, nice to meet you!`, `Hello ${user_name}, I hope you've enjoyed this lab session today!`, `Hey ${user_name}, hope you've had a good day!`];
+    var options = [`Hi there ${user_name}, nice to meet you!`,
+        `Hello ${user_name}, I hope you've enjoyed this lab session today!`,
+        `Hey ${user_name}, hope you've had a good day!`];
     console.log(options[Math.floor(Math.random()*options.length)]);
     return options[Math.floor(Math.random()*options.length)];
 }
-    
+
 
  // --------------- Intents -----------------------
 
@@ -129,10 +51,9 @@ module.exports.handler = (event, context, callback) => {
         // By default, treat the user request as coming from the America/New_York time zone.
         process.env.TZ = 'Pacific/Auckland';
         console.log(`event.bot.name=${event.bot.name}`);
-        
+
         dispatch(event, (response) => callback(null, response));
     } catch (err) {
         callback(err);
     }
 };
-
